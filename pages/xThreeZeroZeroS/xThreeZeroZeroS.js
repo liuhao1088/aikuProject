71790c0a0ce669eb2cf09904753ffba7c1787fef
liuhao1088 <1350569467@qq.com>
2020-11-06 18:23:54 +0800

    更新

--- pages/xThreeZeroZeroS/xThreeZeroZeroS.js
+++ pages/xThreeZeroZeroS/xThreeZeroZeroS.js
@@ -1,4 +1,4 @@
-// pages/drivingRecorder/drivingRecorder.js
+// pages/xThreeZeroZeroS/xThreeZeroZeroS.js
 Page({
 
   /**
@@ -7,11 +7,7 @@ Page({
   data: {
 
   },
-  xThreeZeroZeroSdj: function () {
-    wx.navigateTo({
-      url: "../xThreeZeroZeroS/xThreeZeroZeroS"
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
